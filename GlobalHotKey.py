fb4e9b6557a0513b0ee58b859c857348b87a6e7c
Дмитрий Хижов <flameon1989@yandex.ru>
2020-01-15 21:20:56 +0400

    GHK refactor

--- GlobalHotKey.py
+++ GlobalHotKey.py
@@ -3,40 +3,31 @@ from pynput import keyboard
 
 def hotkey(window):
     """Слушаем в отдельном потоке глобальный хоткей"""
-    COMBINATIONS_1 = [
-        {keyboard.Key.tab, keyboard.KeyCode(char='1')},
-    ]
-
-    COMBINATIONS_2 = [
-        {keyboard.Key.tab, keyboard.KeyCode(char='2')},
-    ]
-    COMBINATIONS_3 = [
-        {keyboard.Key.tab, keyboard.KeyCode(char='3')},
-    ]
-    COMBINATIONS_4 = [
-        {keyboard.Key.tab, keyboard.KeyCode(char='4')},
-    ]
+    COMBO_1 = [keyboard.Key.space, keyboard.KeyCode(char='1')]
+    COMBO_2 = [keyboard.Key.space, keyboard.KeyCode(char='2')]
+    COMBO_3 = [keyboard.Key.space, keyboard.KeyCode(char='3')]
+    COMBO_4 = [keyboard.Key.space, keyboard.KeyCode(char='4')]
     current = set()
 
-    def executeTab1():
+    def execute_space1():
         window.web.exit = False
         window.web.start_data = window.date_start.toPlainText()
         window.web.end_data = window.date_end.toPlainText()
         window.web.url = window.url.toPlainText()
         window.web.add_banner(gui=window)
-        print('\n *** Нажата комбинация клавиш: Tab + 1 \n *** Должна вызваться функция :)')
+        print('\n *** Нажата комбинация клавиш: space + 1 \n *** Должна вызваться функция :)')
 
-    def executeTab2():
+    def execute_space2():
         window.web.parser(gui=window) if not window.web.actions_data else window.web.add_actions(gui=window)
-        print('\n *** Нажата комбинация клавиш: Tab + 2 \n *** Должна вызваться функция :)')
+        print('\n *** Нажата комбинация клавиш: space + 2 \n *** Должна вызваться функция :)')
 
-    def executeTab3():
+    def execute_space3():
         window.web.download_banners(gui=window)
-        print('\n *** Нажата комбинация клавиш: Tab + 3 \n *** Должна вызваться функция :)')
+        print('\n *** Нажата комбинация клавиш: space + 3 \n *** Должна вызваться функция :)')
 
-    def executeTab4():
+    def execute_space4():
         window.web.parser_sephora(gui=window)
-        print('\n *** Нажата комбинация клавиш: Tab + 4 \n *** Должна вызваться функция :)')
+        print('\n *** Нажата комбинация клавиш: space + 4 \n *** Должна вызваться функция :)')
 
     def get_key_name(key):
         """Определяем имя нажатой клавишы"""
@@ -52,34 +43,28 @@ def hotkey(window):
             print('--- Нажата клавиша: {}'.format(key_name))
             window.driver.exit = True
             window.sizer.exit = True
-        elif key == keyboard.Key.tab:
+        elif key == keyboard.Key.space:
+            current.add(key)
             print('--- Нажата клавиша: {}'.format(key_name))
-        elif (key == keyboard.KeyCode.from_char('!')) or (key == keyboard.KeyCode.from_char('@')) or \
-                (key == keyboard.KeyCode.from_char('"')) or (key == keyboard.KeyCode.from_char('#')) or \
-                (key == keyboard.KeyCode.from_char('№')):
+        elif (key == keyboard.KeyCode.from_char('1')) or (key == keyboard.KeyCode.from_char('2')) or \
+                (key == keyboard.KeyCode.from_char('3')) or (key == keyboard.KeyCode.from_char('4')):
             print('--- Нажата клавиша: {}'.format(key_name))
         else:
             print('Информационно. Вы нажали: {}'.format(key_name))
             current.clear()
-        if any([key in COMBO for COMBO in COMBINATIONS_1]):
+        if any([key in COMBO_1 + COMBO_2 + COMBO_3 + COMBO_4]):
             current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS_1):
-            executeTab1()
+        if all(key in current for key in COMBO_1):
+            execute_space1()
             current.clear()
-        if any([key in COMBO for COMBO in COMBINATIONS_2]):
-            current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS_2):
-            executeTab2()
+        elif all(key in current for key in COMBO_2):
+            execute_space2()
             current.clear()
-        if any([key in COMBO for COMBO in COMBINATIONS_3]):
-            current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS_3):
-            executeTab3()
+        elif all(key in current for key in COMBO_3):
+            execute_space3()
             current.clear()
-        if any([key in COMBO for COMBO in COMBINATIONS_4]):
-            current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS_4):
-            executeTab4()
+        elif all(key in current for key in COMBO_4):
+            execute_space4()
             current.clear()
 
     with keyboard.Listener(on_press=on_press) as listener:
