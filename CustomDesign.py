7b3653b9d530b4f8d07d3147fab85727e521f9ab
Дмитрий Хижов <flameon1989@yandex.ru>
2020-01-15 21:25:38 +0400

    CDesign fix ''

--- CustomDesign.py
+++ CustomDesign.py
@@ -12,13 +12,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("DTMainWindow")
+        MainWindow.setObjectName('DTMainWindow')
         MainWindow.resize(869, 489)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setObjectName('centralwidget')
         self.path_window = QtWidgets.QTextBrowser(self.centralwidget)
         self.path_window.setGeometry(QtCore.QRect(10, 30, 681, 31))
-        self.path_window.setObjectName("path_window")
+        self.path_window.setObjectName('path_window')
         self.path_buttom = QtWidgets.QPushButton(self.centralwidget)
         self.path_buttom.setGeometry(QtCore.QRect(700, 30, 140, 31))
 
@@ -26,7 +26,7 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.path_buttom.setFont(font)
-        self.path_buttom.setObjectName("path_buttom")
+        self.path_buttom.setObjectName('path_buttom')
         self.path_buttom.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.data_start_label = QtWidgets.QLabel(self.centralwidget)
@@ -39,15 +39,15 @@ class Ui_MainWindow(object):
         self.data_start_label.setFont(font)
         self.data_start_label.setTextFormat(QtCore.Qt.AutoText)
         self.data_start_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.data_start_label.setObjectName("data_start_label")
+        self.data_start_label.setObjectName('data_start_label')
 
         self.date_start = QtWidgets.QTextEdit(self.centralwidget)
         self.date_start.setGeometry(QtCore.QRect(580, 180, 261, 31))
-        self.date_start.setObjectName("date_start")
+        self.date_start.setObjectName('date_start')
 
         self.date_end = QtWidgets.QTextEdit(self.centralwidget)
         self.date_end.setGeometry(QtCore.QRect(580, 250, 261, 31))
-        self.date_end.setObjectName("date_end")
+        self.date_end.setObjectName('date_end')
 
         self.data_end_label = QtWidgets.QLabel(self.centralwidget)
         self.data_end_label.setGeometry(QtCore.QRect(600, 230, 211, 16))
@@ -56,11 +56,11 @@ class Ui_MainWindow(object):
         font.setUnderline(True)
         self.data_end_label.setFont(font)
         self.data_end_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.data_end_label.setObjectName("data_end_label")
+        self.data_end_label.setObjectName('data_end_label')
 
         self.url = QtWidgets.QTextEdit(self.centralwidget)
         self.url.setGeometry(QtCore.QRect(580, 320, 261, 31))
-        self.url.setObjectName("url")
+        self.url.setObjectName('url')
 
         self.url_label = QtWidgets.QLabel(self.centralwidget)
         self.url_label.setGeometry(QtCore.QRect(620, 300, 191, 16))
@@ -70,7 +70,7 @@ class Ui_MainWindow(object):
         font.setStrikeOut(False)
         self.url_label.setFont(font)
         self.url_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.url_label.setObjectName("url_label")
+        self.url_label.setObjectName('url_label')
 
         self.rename_button = QtWidgets.QPushButton(self.centralwidget)
         self.rename_button.setGeometry(QtCore.QRect(580, 400, 131, 51))
@@ -78,12 +78,12 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.rename_button.setFont(font)
-        self.rename_button.setObjectName("rename_button")
+        self.rename_button.setObjectName('rename_button')
         self.rename_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.rename_checbox = QtWidgets.QCheckBox(self.centralwidget)
         self.rename_checbox.setGeometry(QtCore.QRect(580, 375, 161, 20))
-        self.rename_checbox.setObjectName("rename_checbox")
+        self.rename_checbox.setObjectName('rename_checbox')
 
         self.run_browser = QtWidgets.QPushButton(self.centralwidget)
         self.run_browser.setGeometry(QtCore.QRect(580, 80, 131, 51))
@@ -91,7 +91,7 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.run_browser.setFont(font)
-        self.run_browser.setObjectName("browser")
+        self.run_browser.setObjectName('browser')
         self.run_browser.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.run_ad_browser = QtWidgets.QPushButton(self.centralwidget)
@@ -100,12 +100,12 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.run_ad_browser.setFont(font)
-        self.run_ad_browser.setObjectName("пусто")
+        self.run_ad_browser.setObjectName('пусто')
         self.run_ad_browser.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         self.command_window = QtWidgets.QTextBrowser(self.centralwidget)
         self.command_window.setGeometry(QtCore.QRect(10, 80, 551, 381))
-        self.command_window.setObjectName("command_window")
+        self.command_window.setObjectName('command_window')
 
         self.path_label = QtWidgets.QLabel(self.centralwidget)
         self.path_label.setGeometry(QtCore.QRect(10, 10, 191, 16))
@@ -116,7 +116,7 @@ class Ui_MainWindow(object):
         font.setUnderline(True)
         font.setWeight(50)
         self.path_label.setFont(font)
-        self.path_label.setObjectName("path_label")
+        self.path_label.setObjectName('path_label')
 
         self.resize_buttom = QtWidgets.QPushButton(self.centralwidget)
         self.resize_buttom.setGeometry(QtCore.QRect(720, 400, 131, 51))
@@ -124,12 +124,12 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(75)
         self.resize_buttom.setFont(font)
-        self.resize_buttom.setObjectName("resize_buttom")
+        self.resize_buttom.setObjectName('resize_buttom')
         self.resize_buttom.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setObjectName('statusbar')
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -137,20 +137,20 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("DTMainWindow", "DTMainWindow"))
+        MainWindow.setWindowTitle(_translate('DTMainWindow', 'DTMainWindow'))
         self.path_window.setPlaceholderText(
-            _translate("MainWindow", "Путь до папки с баннерами которые будут переименованы"))
-        self.path_buttom.setText(_translate("MainWindow", "Выбрать папку ..."))
-        self.data_start_label.setText(_translate("MainWindow", "ДАТА НАЧАЛА АКЦИИ:"))
-        self.date_start.setPlaceholderText(_translate("MainWindow", "Пример 01.01.2019, если пусто то текущая дата"))
-        self.date_end.setPlaceholderText(_translate("MainWindow", "Пример 31.12.2019, если есть"))
-        self.data_end_label.setText(_translate("MainWindow", "ДАТА ОКОНЧАНИЯ АКЦИИ:"))
-        self.url_label.setText(_translate("MainWindow", "ССЫЛКА НА АКЦИЮ:"))
-        self.url.setPlaceholderText(_translate("MainWindow", "URL акции если есть"))
-        self.rename_button.setText(_translate("MainWindow", "Переименовать "))
-        self.rename_checbox.setText(_translate("MainWindow", "В одну папку"))
-        self.run_browser.setText(_translate("MainWindow", "Браузер"))
-        self.run_ad_browser.setText(_translate("MainWindow", "Пусто"))
-        self.command_window.setPlaceholderText(_translate("MainWindow", ""))
-        self.path_label.setText(_translate("MainWindow", "Путь до папки с баннерами:"))
-        self.resize_buttom.setText(_translate("MainWindow", "Изменить размер"))
+            _translate('MainWindow', 'Путь до папки с баннерами которые будут переименованы'))
+        self.path_buttom.setText(_translate('MainWindow', 'Выбрать папку ...'))
+        self.data_start_label.setText(_translate('MainWindow', 'ДАТА НАЧАЛА АКЦИИ:'))
+        self.date_start.setPlaceholderText(_translate('MainWindow', 'Пример 01.01.2019, если пусто то текущая дата'))
+        self.date_end.setPlaceholderText(_translate('MainWindow', 'Пример 31.12.2019, если есть'))
+        self.data_end_label.setText(_translate('MainWindow', 'ДАТА ОКОНЧАНИЯ АКЦИИ:'))
+        self.url_label.setText(_translate('MainWindow', 'ССЫЛКА НА АКЦИЮ:'))
+        self.url.setPlaceholderText(_translate('MainWindow', 'URL акции если есть'))
+        self.rename_button.setText(_translate('MainWindow', 'Переименовать'))
+        self.rename_checbox.setText(_translate('MainWindow', 'В одну папку'))
+        self.run_browser.setText(_translate('MainWindow', 'Браузер'))
+        self.run_ad_browser.setText(_translate('MainWindow', 'Пусто'))
+        self.command_window.setPlaceholderText(_translate('MainWindow', ''))
+        self.path_label.setText(_translate('MainWindow', 'Путь до папки с баннерами:'))
+        self.resize_buttom.setText(_translate('MainWindow', 'Изменить размер'))
