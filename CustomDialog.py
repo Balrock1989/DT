037d84df3f584661b6855557b79675eec8d5365e
Дмитрий Хижов <flameon1989@yandex.ru>
2020-01-15 21:26:45 +0400

    CDialog fix ''

--- CustomDialog.py
+++ CustomDialog.py
@@ -12,16 +12,16 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
+        Dialog.setObjectName('Dialog')
         Dialog.resize(280, 200)
 
         self.textEdit = QtWidgets.QLineEdit(Dialog)
         self.textEdit.setGeometry(QtCore.QRect(20, 110, 240, 30))
-        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setObjectName('textEdit')
 
         self.ok = QtWidgets.QPushButton(Dialog)
         self.ok.setGeometry(QtCore.QRect(170, 150, 90, 30))
-        self.ok.setObjectName("ok")
+        self.ok.setObjectName('ok')
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
@@ -30,11 +30,11 @@ class Ui_Dialog(object):
 
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setGeometry(QtCore.QRect(20, 80, 250, 30))
-        self.label.setObjectName("label")
+        self.label.setObjectName('label')
 
         self.label2 = QtWidgets.QLabel(Dialog)
         self.label2.setGeometry(QtCore.QRect(0, 35, 280, 30))
-        self.label2.setObjectName("label2")
+        self.label2.setObjectName('label2')
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
@@ -43,7 +43,7 @@ class Ui_Dialog(object):
 
         self.label3 = QtWidgets.QLabel(Dialog)
         self.label3.setGeometry(QtCore.QRect(0, 5, 280, 30))
-        self.label3.setObjectName("label3")
+        self.label3.setObjectName('label3')
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label3.setFont(font)
@@ -54,8 +54,8 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.ok.setText(_translate("Dialog", "ENTER"))
-        self.label.setText(_translate("Dialog", "Введите размер:"))
-        self.label2.setText(_translate("Dialog", "Размеры"))
-        self.label3.setText(_translate("Dialog", "Текущий размер:"))
+        Dialog.setWindowTitle(_translate('Dialog', 'Dialog'))
+        self.ok.setText(_translate('Dialog', 'ENTER'))
+        self.label.setText(_translate('Dialog', 'Введите размер:'))
+        self.label2.setText(_translate('Dialog', 'Размеры'))
+        self.label3.setText(_translate('Dialog', 'Текущий размер:'))
