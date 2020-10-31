d8cca6985a02873985fc3b420b3ed3deac9931af
SMOKE <flameon1989@yandex.ru>
2020-10-31 11:08:03 +0400

    auth example

---
+++ auth_example.py
@@ -0,0 +1,11 @@
+auth_url_dt = "https://login.****.com/public/aLogin.action"
+coupun_url = "https://login.****.com/pan/mCreateVouchers.action?programId="
+username_dt = 'o***@co**.d***.ru'
+password_dt = '*****'
+
+username_ad = "*****"
+password_ad = "******"
+auth_url_ad = "https://www.admitad.com/ru/sign_in/"
+coupon_ad = "https://www.admitad.com/ru/webmaster/websites/1079416/"
+
+CREDENTIALS_FILE = '**********.json'
