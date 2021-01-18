e4aeba0f99f449bc145bcae3d2cebc4806ed3092
SMOKE <flameon1989@yandex.ru>
2021-01-18 14:10:10 +0400

    fix rozetka ua-ru

--- parsers/process_rozetka.py
+++ parsers/process_rozetka.py
@@ -1,6 +1,9 @@
 import re
 from multiprocessing import Process
 
+import requests
+from bs4 import BeautifulSoup
+
 import helpers.helper as helper
 from database.data_base import actions_exists_in_db
 
@@ -19,9 +22,17 @@ class Rozetka_process(Process):
         partner_name = 'Розетка'
         actions_data = []
         self.queue.put(f'set 20')
+        s = requests.Session()
+        cookie = s.get('https://rozetka.com.ua/news-articles-promotions/promotions/').request.headers.get(
+            'cookie').replace('slang=ua', 'slang=ru')
+        s.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+            'Cookie': cookie
+        })
         for i in range(1, 21):
-            main_url = f'https://rozetka.com.ua/news-articles-promotions/promotions/page={i}'
-            page = helper.get_page_use_request(main_url)
+            main_url = f'https://rozetka.com.ua/news-articles-promotions/promotions/page={i}/'
+            request = s.get(main_url)
+            page = BeautifulSoup(request.text, 'lxml')
             divs = page.find_all('div', class_='promo-cat-i')
             for div in divs:
                 if div.get('name') == 'more_promotions':
