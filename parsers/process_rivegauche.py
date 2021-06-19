d3aee7845217784023e8c7315251aa885d1b766d
SMOKE <flameon1989@yandex.ru>
2021-06-19 19:53:29 +0400

    fix rivegauche

--- parsers/process_rivegauche.py
+++ parsers/process_rivegauche.py
@@ -18,32 +18,28 @@ class Rivegauche_process(Process):
         partner_name = 'Ривгош'
         actions_data = []
         base_url = 'http://www.rivegauche.ru'
-        for i in range(0, 2):
-            main_url = f'http://www.rivegauche.ru/action?page={i}'
-            try:
-                page = helper.get_page_use_request(main_url)
-            except:
+        main_url = f'https://rivegauche.ru/promotionslist'
+        page = helper.get_page_use_webdriver(main_url, scroll=True, hidden=True)
+        divs = page.select('.b-promo-item')
+        self.queue.put(f'set {len(divs)}')
+        for div in divs:
+            url = base_url + div.find('a').get('href')
+            name = div.text.strip()
+            name = re.sub(r'\n', ' ', name).strip()
+            start = helper.get_first_day_month()
+            end = helper.get_date_end_month()
+            code = "Не требуется"
+            desc = name
+            short_desc = ''
+            action_type = helper.check_action_type(code, name, desc)
+            if helper.promotion_is_outdated(end):
+                self.queue.put('progress')
                 continue
-            divs = page.find_all('div', class_='name')
-            self.queue.put(f'set {len(divs)}')
-            for div in divs:
-                url = base_url + div.find('a').get('href')
-                name = div.text.strip()
-                name = re.sub(r'\n', ' ', name).strip()
-                start = helper.get_first_day_month()
-                end = helper.get_date_end_month()
-                code = "Не требуется"
-                desc = name
-                short_desc = ''
-                action_type = helper.check_action_type(code, name, desc)
-                if helper.promotion_is_outdated(end):
+            if not self.ignore:
+                if actions_exists_in_db(partner_name, name, start, end):
                     self.queue.put('progress')
                     continue
-                if not self.ignore:
-                    if actions_exists_in_db(partner_name, name, start, end):
-                        self.queue.put('progress')
-                        continue
-                action = helper.generate_action(partner_name, name, start, end, desc, code, url, action_type,short_desc)
-                actions_data.append(action)
-                self.queue.put('progress')
+            action = helper.generate_action(partner_name, name, start, end, desc, code, url, action_type, short_desc)
+            actions_data.append(action)
+            self.queue.put('progress')
         helper.filling_queue(self.queue, actions_data, partner_name)
